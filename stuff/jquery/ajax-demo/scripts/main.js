ac89dd647278302643bad5798708bef9e53ca3b0
manuelbarzi <manuelbarzi@gmail.com>
2017-10-03 21:24:27 +0200

    update files

--- stuff/jquery/ajax-demo/scripts/main.js
+++ stuff/jquery/ajax-demo/scripts/main.js
@@ -1,24 +1,24 @@
-function successCallback(result) {
-    //console.log(result);
-
-    var ul = $('ul');
-
-    result.forEach(function(item) {
-      ul.append('<li>' + item.id + ' ' + item.first_name + '</li>');
-    });
-}
-
-function errorCallback(err) {
-    console.error(err);
-}
-
 var params = {
     url: 'data/employees.json',
-    mimeType: 'application/json',
-    success: successCallback,
-    error: errorCallback
+    dataType: 'json',
+    success: onSuccess,
+    error: onError
 }
 
 $('button').click(function() {
     $.ajax(params);
 });
+
+function onSuccess(results) {
+    if(results.length) {
+        var ul = $('ul');
+
+        results.forEach(function(item) {
+            ul.append('<li>' + item.id + ' ' + item.first_name + '</li>');
+        });
+    }
+}
+
+function onError(err) {
+    console.error(err);
+}
