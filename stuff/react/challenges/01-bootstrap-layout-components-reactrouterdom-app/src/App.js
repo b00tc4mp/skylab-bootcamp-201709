c1d865ba2cf103d6f91c050aad299224fa1c7258
manuelbarzi <manuelbarzi@gmail.com>
2017-10-12 16:57:01 +0200

    add react router dom challenge solution

--- stuff/react/challenges/01-bootstrap-layout-components-reactrouterdom-app/src/App.js
+++ stuff/react/challenges/01-bootstrap-layout-components-reactrouterdom-app/src/App.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, {Component} from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Jumbotron from './components/Jumbotron'
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import ContactPage from "./components/ContactPage";
 
 class App extends Component {
     render() {
-        return <HelloWorld/>
+        return <BrowserRouter>
+            <div className="container">
+                <NavBar/>
+                <Route exact path="/" component={Jumbotron}/>
+                <Route exact path="/" component={HomePage}/>
+                <Route path="/about" component={AboutPage}/>
+                <Route path="/contact" component={ContactPage}/>
+            </div>
+        </BrowserRouter>
     }
 }
 
---
+++ stuff/react/challenges/01-bootstrap-layout-components-reactrouterdom-app/src/components/ContactPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ContactPage extends Component {
+
+    render() {
+        return <div>
+            <h1>Contact Page</h1>
+            <p>This is the contact page</p>
+        </div>
+    }
+}
+
+export default ContactPage
