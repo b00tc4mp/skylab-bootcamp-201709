f34e4eb25c40ac3b8f667d3a6f95cbfb3eaf725b
manuelbarzi <manuelbarzi@gmail.com>
2017-11-07 16:58:57 +0100

    add helloworld api expresss auth token

---
+++ stuff/mongo/demos-3/helloworld-api-express-auth-token/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, proceed) => {
+	const auth = req.get('authorization') // APIKEY <adsfasdfasfas...>
+
+	let apikey
+
+	if (auth && ((apikey = auth.split(' ')).length === 2) && apikey[0] === 'APIKEY' && apikey[1] === '123')
+		proceed()
+	else 
+		res.json({
+			status: 'KO',
+			message: 'Not authorized'
+		})
+})
+
+app.get('/', (req, res) => res.json({ 
+	status: 'OK',
+	message: 'Hello, World!'
+}))
+
+console.log('Starting HelloWorld API...')
+
+app.listen(process.env.PORT, () => console.log(`HelloWorld API is up on port ${process.env.PORT}`))
+
+process.on('SIGINT', () => {
+    console.log('\nStopping HelloWorld API...')
+
+    process.exit()
+})
