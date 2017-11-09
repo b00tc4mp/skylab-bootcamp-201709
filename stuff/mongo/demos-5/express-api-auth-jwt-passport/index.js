43baedaf6468a723fe48148ec7bf233edaf5c361
manuelbarzi <manuelbarzi@gmail.com>
2017-11-09 12:44:26 +0100

    add express api auth jwt mongoose passport

---
+++ stuff/mongo/demos-5/express-api-auth-jwt-passport/index.js
@@ -0,0 +1,71 @@
+require('dotenv').config()
+
+require('./mongoose')(process.env.DB_URL)
+
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.use(require('./passport')(process.env.SECRET))
+
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const User = require('./users/UserModel')
+
+const auth = express.Router()
+
+auth.post('/register', (req, res) => {
+    const { username, password } = req.body
+
+    const user = new User({ username })
+
+    User.register(user, password, err => {
+        if (err) return res.json({
+            status: 'KO',
+            message: err.message
+        })
+
+        res.json({
+            status: 'OK',
+            message: 'user registered successfully'
+        })
+    })
+})
+
+auth.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
+    const { _id: id, username } = req.user
+
+    const token = jwt.sign({ id, username }, process.env.SECRET)
+
+    res.json({
+        status: 'OK',
+        message: 'user authenticated successfully',
+        data: token
+    })
+})
+
+app.use('/auth', auth)
+
+const api = express.Router()
+
+api.use(passport.authenticate('jwt', { session: false }))
+
+api.route('/helloworld')
+    .get((req, res) => res.json({ 
+        status: 'OK',
+        message: 'Hello, World!'
+    }))
+
+app.use('/api', api)
+
+console.log('Starting Express API...')
+
+app.listen(process.env.PORT, () => console.log(`Express API is up on port ${process.env.PORT}`))
+
+process.on('SIGINT', () => {
+    console.log('\nStopping Express API...')
+
+    process.exit()
+})
